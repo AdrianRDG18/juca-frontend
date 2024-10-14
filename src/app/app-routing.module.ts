0fe2d0fa92fccbdefb3aa78188c0e72ce7841d3d
AdrianRDG18 <kersdeg18@gmail.com>
2024-10-14 11:47:08 -0600

    Feature: add pages routing & module, and page-not-found and set the first routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NoPageFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+
+import { PagesComponent } from './pages.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    NoPageFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule
+  ]
+})
+export class PagesModule { }
