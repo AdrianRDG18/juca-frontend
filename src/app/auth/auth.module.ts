c03b719eee3ce3ef3a931c8c9edaee59706e1403
AdrianRDG18 <kersdeg18@gmail.com>
2024-10-23 11:21:43 -0600

    Feature: add /auth routing module and module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,10 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedModule
+]
 })
 export class AuthModule { }
