298743f19845c8c638669a43f0bd300e30e9949b
AdrianRDG18 <kersdeg18@gmail.com>
2024-10-23 11:28:30 -0600

    Feature: create language selector component and add it to login component

---
+++ src/app/shared/language-selector/language-selector.component.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrl: './language-selector.component.css'
+})
+export class LanguageSelectorComponent {
+
+  languages = signal([
+    { code: 'en', flag: '🇺🇸' },
+    { code: 'es', flag: '🇪🇸' },
+    { code: 'fr', flag: '🇫🇷' },
+    { code: 'it', flag: '🇮🇹' },
+  ]);
+
+}
