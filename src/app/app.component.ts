85c90fd9b4267237d72e9dc7185db28dc7eb53b6
AdrianRDG18 <kersdeg18@gmail.com>
2024-10-29 09:05:08 -0600

    Feature: add effect fucntion to check/set language value when the app is loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { LanguageService } from './services/language.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'juca-frontend';
+
+  cookie =  inject(CookieService);
+  languageService = inject(LanguageService);
+
+  cookieLogEffect = effect(() => {
+    console.log({ cookie: this.cookie.get('lang')});
+    const lang = this.cookie.check('lang') ? this.cookie.get('lang') : 'en';
+    this.languageService.changeLanguage(lang);
+  });
+
 }
