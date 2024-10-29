9268da8ad14415e6d0edddef0bcf9adf4f292565
AdrianRDG18 <kersdeg18@gmail.com>
2024-10-29 08:58:37 -0600

    Feature: add ngx-translate & ngx-translate/http-loader libs and add refs in sharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LanguageSelectorComponent } from './language-selector/language-selector.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     LanguageSelectorComponent
@@ -10,7 +16,14 @@ import { LanguageSelectorComponent } from './language-selector/language-selector
     LanguageSelectorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ]
 })
-export class SharedModule { }
+export class SharedModule{}
