f2d5416eeb494be440d12a8e82dc928f2a7fcef4
AdrianRDG <adrian.rojas@tergar.org>
2025-01-17 17:53:16 -0600

    Update: add PagesModule to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
 import { HttpClientModule } from '@angular/common/http';
+
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
