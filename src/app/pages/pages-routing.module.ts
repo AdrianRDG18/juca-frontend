9db4d5b1d07cd9b7a17c623ae662c02d4a477728
AdrianRDG <adrian.rojas@tergar.org>
2025-01-17 17:26:22 -0600

    Update: add isAuth Guard to /dashboard route

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
+import { isAuthGuard } from '../guards/is-auth.guard';
 
 const routes: Routes = [
-  { path: 'dashboard', 
-    component: PagesComponent, 
+  { path: 'dashboard',
+    canActivate: [ isAuthGuard ],
+    component: PagesComponent,
     children: [
-      
+
     ]
   }
 ];
