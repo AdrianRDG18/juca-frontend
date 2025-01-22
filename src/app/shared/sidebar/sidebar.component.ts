ea5bb158c752c41502fbc3fb39f5ac82e8d8bf0b
AdrianRDG <adrian.rojas@tergar.org>
2025-01-21 21:05:49 -0600

    Update: add elements to sidebar &  add dynamic menu and submenu

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user.model';
+import { SweetAlertService } from '../../services/swal.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  public user?: User;
+
+  menu: any[] = [
+    {
+      title: 'Admin',
+      icon: 'mdi mdi-folder-lock-open',
+      role_required: 'ADMIN_ROLE',
+      submenu: [
+        {
+          title: 'Users',
+          route: 'users',
+          role_required: 'ADMIN_ROLE'
+        }
+      ]
+    }
+  ]
+
+  constructor(private _authService: AuthService,
+              private _swal: SweetAlertService,
+              private _router: Router
+  ){
+    this.user = this._authService.user;
+  }
+  
+  logout(){
+    this._swal.swalConfirm('Logout', 'Are you sure to close the session of this user?')
+        .then( (resp) => {
+          if(resp.isConfirmed){
+            localStorage.removeItem('token');
+            this._router.navigateByUrl('/login');
+          }
+        });
+  }
 
 }
