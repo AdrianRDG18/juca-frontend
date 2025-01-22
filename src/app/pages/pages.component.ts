1b12adfcb96fd64bf9178beb15ccc4577f8c0898
AdrianRDG <adrian.rojas@tergar.org>
2025-01-21 20:41:42 -0600

    IMPORTANT: put on all initializations inside custom.min.js in a function to inicializate on pages.component and the theme works as normal

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare function customInitStyleFunctions(): any;
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: ``
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+
+  ngOnInit(): void {
+    customInitStyleFunctions();
+  }
 
 }
