f27a4309685f8e759fc960590cb73d5bff539fe3
AdrianRDG <adrian.rojas@tergar.org>
2025-01-22 10:51:49 -0600

    Update: breadcrumbs with dynamic title and app tittle taken from route

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './breadcrumbs.component.css'
 })
 export class BreadcrumbsComponent {
+
+  title: string = '';
+  titleSubs$: Subscription;
+
+  constructor(private router: Router) {
+    this.titleSubs$ = this.extractTitleRoute()
+                          .subscribe( ({title}) => {
+                            this.title = title;
+                            document.title = `JUCA - ${title}`;
+                          });
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
+  }
+
+  extractTitleRoute(){
+    return this.router.events
+      .pipe(
+        filter( (event: any) => event instanceof ActivationEnd),
+        filter( (event:ActivationEnd) => event.snapshot.firstChild === null ),
+        map( (event: ActivationEnd) => event.snapshot.data)
+      )
+  }
 
 }
