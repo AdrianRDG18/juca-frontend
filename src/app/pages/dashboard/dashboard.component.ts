58cac47c43f039c6fb00b1c43b70b0e6d86efae5
AdrianRDG <adrian.rojas@tergar.org>
2025-01-31 13:10:57 -0600

    Fix: add settingsService to dashboard.component to init the theme functions

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(private _settingsService: SettingsService){}
 }
