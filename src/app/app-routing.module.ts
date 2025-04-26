2aa832aaffd96778d6fff9a2675df4842b78a485
AdrianRDG <adrian.rojas@tergar.org>
2025-04-26 09:41:10 -0600

    !Feat: add register component and route with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { PagesRoutingModule } from './pages/pages-routing';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./auth/login/login.module').then( m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) },
   { path: '**', component: NoPageFoundComponent }
 ];
 
