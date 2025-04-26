1765f54260ec057c44b8bfd10706b553e2c99f57
AdrianRDG <adrian.rojas@tergar.org>
2025-04-26 09:42:53 -0600

    Update: add language selector in register.component and TranslateModule.forChild() to |translate pipe can work correctly

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
+import { SharedModule } from '../../shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { RegisterComponent } from './register.component';
   ],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    SharedModule,
+    TranslateModule.forChild()
   ]
 })
 export class RegisterModule { }
