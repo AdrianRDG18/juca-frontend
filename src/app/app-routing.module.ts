8a92690b74dd734397231f5de423cf2b393edb77
AdrianRDG <kersdeg18@gmail.com>
2025-04-28 11:13:54 -0600

    Update: add pathMatch: full to redirect user to login when the project starts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: () => import('./pages/pages.module').then( m => m.PagesModule) },
   { path: 'login', loadChildren: () => import('./auth/login/login.module').then( m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) },
