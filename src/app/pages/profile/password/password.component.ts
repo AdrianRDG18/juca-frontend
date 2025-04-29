fbce3f8045c37e355200225f5b440b670958adcb
AdrianRDG <kersdeg18@gmail.com>
2025-04-28 21:14:41 -0600

    Feat: change password form, translations, get control errors

--- src/app/pages/profile/password/password.component.ts
+++ src/app/pages/profile/password/password.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-password',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class PasswordComponent {
+
+  translateService = inject(TranslateService);
+  formSubmited: boolean = false;
+
+  passwordForm = new FormGroup({
+    current_pw: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    new_pw: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    conf_new_pw: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  updatePassword(){
+    console.log(this.passwordForm.value);
+    this.formSubmited = true;
+
+  }
+
+  getControlErrors(control_name: string, display_name: string): string[] {
+    const control = this.passwordForm?.get(control_name);
+    if (control?.errors) {
+      return Object.keys(control.errors)
+                   .map( (errorKey: string) => {
+                     return this.getErrorMessage(errorKey, control.errors?.[errorKey], display_name)
+                   });
+    }
+    return [];
+  }
+
+  private getErrorMessage(errorKey: string, errorValue: any, display_name: string): string {
+    const errorMessages: { [key: string]: (errorValue: any) => string } = {
+      // translateService.instant('msg') to translate messages 
+      'required': () => `${display_name} ${this.translateService.instant('errors.is-required')}`,
+      'minlength': () => `${display_name} ${this.translateService.instant('errors.must-be')} ${errorValue.requiredLength} ${this.translateService.instant('errors.characters-long')}`
+    };
+    return errorMessages[errorKey] ? errorMessages[errorKey](errorValue) : `Unknown error: ${errorKey}`;
+  }
 
 }
