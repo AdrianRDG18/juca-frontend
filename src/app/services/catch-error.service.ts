e06f43d27e10baa7df32374aa9181d76189e33d2
AdrianRDG <kersdeg18@gmail.com>
2025-04-30 17:53:45 -0600

    Update: add option to throw if there are an array of errors in CatchErrorService

--- src/app/services/catch-error.service.ts
+++ src/app/services/catch-error.service.ts
@@ -13,11 +13,21 @@ export class CatchErrorService {
     if(Object.keys(error).indexOf('error') > -1) {
 
       if(Object.keys(error.error).indexOf('errors') > -1) {
+
         //It could be a validations error
         const fields = Object.keys(error.error.errors);
         fields.forEach(element => {
           errors += `<strong>${element}</strong>: ${error.error.errors[element].msg} <br>`;
         });
+
+      } else if(Object.keys(error.error).indexOf('error') > -1) {
+
+        const fields = Object.keys(error.error.error);
+        console.log(fields);
+        fields.forEach(element => {
+          errors += `<strong>${element}</strong>: ${error.error.error[element]} <br>`;
+        });
+
       }else if(Object.keys(error.error).indexOf('msg') > -1 ){
         errors = `<strong>Error:</strong> ${error.error.msg}`;
       }else{
